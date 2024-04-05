a408899bff424ff584effe157d90527a06edce89
3akrot <105954803+3akrot@users.noreply.github.com>
2024-04-05 02:18:47 +0200

    new

--- src/main/java/com/example/employeemangmentapp/HelloController.java
+++ src/main/java/com/example/employeemangmentapp/HelloController.java
@@ -1,14 +1,30 @@
 package com.example.employeemangmentapp;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button close;
+
+    @FXML
+    private Button login;
+
+    @FXML
+    private AnchorPane main_form;
+
+    @FXML
+    private TextField password;
+
+    @FXML
+    private TextField username;
+    //this method for closing the app
+    public  void close(){
+
+        System.exit(0);
     }
 }
